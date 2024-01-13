c3047bcef2643f5d24061fdf75beaa18fa4f7249
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-14 00:43:48 +0300

    feat: add functionlity for signup api endpoint

--- auth/internal/routes/signin.go
+++ auth/internal/routes/signin.go
@@ -4,14 +4,68 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/Tibz-Dankan/reserve-now-microservices/internal/models"
+	"github.com/Tibz-Dankan/reserve-now-microservices/internal/services"
 	"github.com/gorilla/mux"
-	// "github.com/Tibz-Dankan/reserve-now-microservices/internal/services"
 )
 
 func signIn(w http.ResponseWriter, r *http.Request) {
 
+	user := models.User{}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		services.AppError(err.Error(), 400, w)
+		return
+	}
+
+	password := user.Password
+
+	user, err = user.FindByEMail(user.Email)
+	if err != nil {
+		services.AppError(err.Error(), 500, w)
+		return
+	}
+
+	if user.ID == 0 {
+		services.AppError("Email is not registered!", 400, w)
+		return
+	}
+
+	passwordMatches, err := user.PasswordMatches(password)
+	if err != nil {
+		services.AppError(err.Error(), 500, w)
+		return
+	}
+
+	if !passwordMatches {
+		services.AppError("Invalid password!", 400, w)
+		return
+	}
+
+	accessToken, err := services.SignJWTToken(user.ID)
+	if err != nil {
+		services.AppError(err.Error(), 500, w)
+		return
+	}
+
+	userMap := map[string]interface{}{
+		"id":      user.ID,
+		"name":    user.Name,
+		"email":   user.Email,
+		"role":    user.Role,
+		"country": user.Country,
+	}
+	response := map[string]interface{}{
+		"status":      "success",
+		"message":     "Sign in successfully",
+		"accessToken": accessToken,
+		"user":        userMap,
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"status": "success", "message": "Sign in successfully"})
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
 
 func SignInRoute(router *mux.Router) {
