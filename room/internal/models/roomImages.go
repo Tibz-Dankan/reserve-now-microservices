2a65a29ff8ce7eb9d89ee0d0b18267227621add8
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-19 02:03:09 +0300

    feat: save url and image path of the image upload in db

---
+++ room/internal/models/roomImages.go
@@ -0,0 +1,49 @@
+package models
+
+func (r *RoomImage) Create(roomImage RoomImage) (int, error) {
+	err := db.Create(&roomImage).Error
+
+	if err != nil {
+		return 0, err
+	}
+
+	return roomImage.ID, nil
+}
+
+func (ri *RoomImage) FindOne(id int) (*RoomImage, error) {
+	var roomImage *RoomImage
+	err := db.First(&roomImage, id).Error
+	if err != nil {
+		return roomImage, err
+	}
+
+	return roomImage, nil
+}
+
+func (ri *RoomImage) FindAll() ([]RoomImage, error) {
+	var roomImages []RoomImage
+	err := db.Find(&roomImages).Error
+	if err != nil {
+		return roomImages, err
+	}
+
+	return roomImages, nil
+}
+
+func (ri *RoomImage) Update(id int) error {
+	ri.ID = id
+	if err := db.Save(&ri).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ri *RoomImage) Delete(id int) error {
+	err := db.Delete(&RoomImage{}, "roomId = ?", ri.ID).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
