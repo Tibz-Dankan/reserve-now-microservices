3896aabd27215eaa70238cb3aa72c74db1761958
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-19 17:30:46 +0300

    feat: create all db tabled for the rooms

--- room/internal/models/schema.go
+++ room/internal/models/schema.go
@@ -1,63 +1,26 @@
 package models
 
 import (
-	"database/sql/driver"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"time"
 
 	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 )
-
-type JSON json.RawMessage
-
-// Scan value into Jsonb, implements sql.Scanner interface
-func (j *JSON) Scan(value interface{}) error {
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
-	}
-
-	result := json.RawMessage{}
-	err := json.Unmarshal(bytes, &result)
-	*j = JSON(result)
-	return err
-}
-
-// Value return json value, implement driver.Valuer interface
-func (j JSON) Value() (driver.Value, error) {
-	if len(j) == 0 {
-		return nil, nil
-	}
-	return json.RawMessage(j).MarshalJSON()
-}
-
-func (JSON) GormDBDataType(db *gorm.DB, field *schema.Field) string {
-	switch db.Dialector.Name() {
-	case "mysql", "sqlite":
-		return "JSON"
-	case "postgres":
-		return "JSONB"
-	}
-	return ""
-}
 
 type Room struct {
 	ID              int            `gorm:"column:id;primaryKey;autoIncrement"`
 	RoomName        string         `gorm:"column:roomName;unique;not null;index"`
 	RoomType        string         `gorm:"column:roomType;not null;index"` //single, double, suite, deluxe, etc
-	Capacity        JSON           `gorm:"column:capacity;not null"`       // format { adults: 2, children: 1 }
 	IsAvailable     bool           `gorm:"column:isAvailable;default:TRUE"`
 	OccupancyStatus string         `gorm:"column:occupancyStatus;enum('vacant', 'occupied', 'undergoing cleaning/maintenance');default:'vacant';not null"`
-	Amenities       JSON           `gorm:"column:amenities;"`
 	View            string         `gorm:"column:view"`
-	Price           JSON           `gorm:"column:price;not null"` //format {amount:50, currency:"US dollar" }
 	Policy          string         `gorm:"column:policy"`
 	AdditionalNotes string         `gorm:"column:additionalNotes"`
-	Publish         JSON           `gorm:"column:publish;default:'{\"isPublished\": false, \"publishedAt\": \"\"}'"`
 	RoomImages      []RoomImage    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	RoomCapacity    RoomCapacity   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	RoomPrice       RoomPrice      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	RoomPublicity   RoomPublicity  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	RoomAmenities   []RoomAmenity  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	RoomBeds        []RoomBed      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	CreatedAt       time.Time      `gorm:"column:createdAt"`
 	UpdatedAt       time.Time      `gorm:"column:updatedAt"`
 	DeletedAt       gorm.DeletedAt `gorm:"column:deletedAt;index"`
@@ -65,11 +28,67 @@ type Room struct {
 
 type RoomImage struct {
 	ID        int            `gorm:"column:id;primaryKey;autoIncrement"`
-	RoomID    int            `gorm:"column:roomId;not null; index"`
+	RoomID    int            `gorm:"column:roomId;not null;index"`
 	ViewType  string         `gorm:"column:viewType;enum('interior', 'exterior', 'bathroom');default:'interior';not null"`
 	URL       string         `gorm:"column:url;not null"`
 	Path      string         `gorm:"column:path;not null"`
 	CreatedAt time.Time      `gorm:"column:createdAt"`
 	UpdatedAt time.Time      `gorm:"column:updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deletedAt;index"`
+}
+
+type RoomCapacity struct {
+	ID        int            `gorm:"column:id;primaryKey;autoIncrement"`
+	RoomID    int            `gorm:"column:roomId;not null;index"`
+	Adults    int            `gorm:"column:adults;not null;"`
+	Children  int            `gorm:"column:children;not null;"`
+	CreatedAt time.Time      `gorm:"column:createdAt"`
+	UpdatedAt time.Time      `gorm:"column:updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deletedAt;index"`
+}
+
+type RoomPrice struct {
+	ID        int            `gorm:"column:id;primaryKey;autoIncrement"`
+	RoomID    int            `gorm:"column:roomId;not null;index"`
+	Amount    int            `gorm:"column:amount;not null;"`
+	Currency  string         `gorm:"column:currency;not null;"`
+	CreatedAt time.Time      `gorm:"column:createdAt"`
+	UpdatedAt time.Time      `gorm:"column:updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deletedAt;index"`
+}
+
+type RoomPublicity struct {
+	ID          int            `gorm:"column:id;primaryKey;autoIncrement"`
+	RoomID      int            `gorm:"column:roomId;not null;index"`
+	IsPublished bool           `gorm:"column:isPublished;default:FALSE;"`
+	CreatedAt   time.Time      `gorm:"column:createdAt"`
+	UpdatedAt   time.Time      `gorm:"column:updatedAt"`
+	DeletedAt   gorm.DeletedAt `gorm:"column:deletedAt;index"`
+}
+
+type RoomAmenity struct {
+	ID        int            `gorm:"column:id;primaryKey;autoIncrement"`
+	RoomID    int            `gorm:"column:roomId;not null;index"`
+	AmenityID int            `gorm:"column:amenityId;not null;index"`
+	CreatedAt time.Time      `gorm:"column:createdAt"`
+	UpdatedAt time.Time      `gorm:"column:updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deletedAt;index"`
+}
+
+type RoomBed struct {
+	ID        int            `gorm:"column:id;primaryKey;autoIncrement"`
+	RoomID    int            `gorm:"column:roomId;not null;index"`
+	BedType   string         `gorm:"column:bedType;not null;"`
+	CreatedAt time.Time      `gorm:"column:createdAt"`
+	UpdatedAt time.Time      `gorm:"column:updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deletedAt;index"`
+}
+
+type Amenity struct {
+	ID            int            `gorm:"column:id;primaryKey;autoIncrement"`
+	item          string         `gorm:"column:bedType;not null;"`
+	RoomAmenities []RoomAmenity  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	CreatedAt     time.Time      `gorm:"column:createdAt"`
+	UpdatedAt     time.Time      `gorm:"column:updatedAt"`
+	DeletedAt     gorm.DeletedAt `gorm:"column:deletedAt;index"`
 }
